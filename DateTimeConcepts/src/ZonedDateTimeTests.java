5c476098c66d599c101a20b8120bbf18413a1172
sfox <kempofighter@gmail.com>
2018-12-08 19:20:24 -0800

    Add additional test involving local and zoned Date and Time objects.

--- DateTimeConcepts/src/ZonedDateTimeTests.java
+++ DateTimeConcepts/src/ZonedDateTimeTests.java
@@ -1,5 +1,7 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDateTime;
+import java.time.Month;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
@@ -15,6 +17,27 @@ class ZonedDateTimeTests {
 		ZonedDateTime zdt = ZonedDateTime.of(2018, 12,  25,  8,  0,  0,  0,  ZoneId.of("America/Los_Angeles"));//ZoneId.SHORT_IDS.get("PST"));
 		System.out.println(zdt.toString());
 		assertEquals(ZoneOffset.ofHours(-8), zdt.getOffset());
+	}
+	
+	@Test
+	void testCombineLocalDateTimeAndZone() {
+		// Leaving from San Francisco on July 20, 2013, at 7:30 p.m.
+		LocalDateTime leaving = LocalDateTime.of(2013, Month.JULY, 20, 19, 30);
+		ZoneId leavingZone = ZoneId.of("America/Los_Angeles"); 
+		ZonedDateTime departure = ZonedDateTime.of(leaving, leavingZone);
+		System.out.println(departure.toString());
+		
+		// Create  new object with same time but CST.  Notice that this really does just change the zone
+		// and doesn't adjust the time to the new zone.
+		ZonedDateTime chicago = leaving.atZone(ZoneId.of("America/Chicago"));
+		assertEquals(19, chicago.getHour());
+		
+		// The withZoneSameInstant actually causes the time to change.  Instead of simply changing the zone itself
+		// it is adjusting the instant of time based on temporal between the zones.
+		ZoneId arrivingZone = ZoneId.of("Asia/Tokyo"); 
+		ZonedDateTime arrival = departure.withZoneSameInstant(arrivingZone);
+		System.out.println(arrival.toString());
+		assertEquals(11, arrival.getHour());
 	}
 
 }
