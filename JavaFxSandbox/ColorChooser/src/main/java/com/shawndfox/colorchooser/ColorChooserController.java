925ac703aa41ddfa485ef8ef044dd8c75219bbdd
sfox <kempofighter@gmail.com>
2019-08-17 11:06:25 -0700

    Commit first version of the color chooser app with the basic UI layout designed.

---
+++ JavaFxSandbox/ColorChooser/src/main/java/com/shawndfox/colorchooser/ColorChooserController.java
@@ -0,0 +1,33 @@
+package com.shawndfox.colorchooser;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+
+public class ColorChooserController {
+
+    @FXML
+    private Slider redSlider;
+
+    @FXML
+    private Slider greenSlider;
+
+    @FXML
+    private Slider blueSlider;
+
+    @FXML
+    private Slider alphaSlider;
+
+    @FXML
+    private TextField redTextField;
+
+    @FXML
+    private TextField greenTextField;
+
+    @FXML
+    private TextField blueTextField;
+
+    @FXML
+    private TextField alphaTextField;
+
+}
