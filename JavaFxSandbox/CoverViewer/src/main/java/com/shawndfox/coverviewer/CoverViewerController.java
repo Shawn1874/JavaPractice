7c068facfd54b6d3e525972cb5ebcc36ae0ad562
sfox <kempofighter@gmail.com>
2019-08-17 16:40:29 -0700

    Commit the initial version of the CoverViewer app with only the basic UI layout finished.

---
+++ JavaFxSandbox/CoverViewer/src/main/java/com/shawndfox/coverviewer/CoverViewerController.java
@@ -0,0 +1,17 @@
+package com.shawndfox.coverviewer;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class CoverViewerController implements Initializable {
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+}
