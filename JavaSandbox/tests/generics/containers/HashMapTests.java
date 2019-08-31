a54dfd03d82f91fca0d28aa7b1893323e2a9a424
sfox <kempofighter@gmail.com>
2019-08-30 19:25:57 -0700

    Commit some place holder files for JUnit tests, and a couple of concurrency examples.

---
+++ JavaSandbox/tests/generics/containers/HashMapTests.java
@@ -0,0 +1,13 @@
+package generics.containers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Collections;
+
+import class_concepts.Planet;
+
+import org.junit.jupiter.api.Test;
+
+public class HashMapTests {
+
+}
---
+++ JavaSandbox/tests/generics/containers/HashSetTests.java
@@ -0,0 +1,5 @@
+package generics.containers;
+
+public class HashSetTests {
+
+}
