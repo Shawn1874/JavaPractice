3a9808e41a7a00c5378e0070d0e881b7d58aff40
sfox <kempofighter@gmail.com>
2019-09-08 18:13:20 -0700

    Commit initial version of project as a basic hello world app.

---
+++ JavaFxSandbox/JavaFxJDBCMovies/src/test/java/com/shawndfox/moviesdb/TableModelTests.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.shawndfox.moviesdb;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Shawn D. Fox
+ */
+public class TableModelTests
+{
+   
+   public TableModelTests()
+   {
+   }
+   
+}
