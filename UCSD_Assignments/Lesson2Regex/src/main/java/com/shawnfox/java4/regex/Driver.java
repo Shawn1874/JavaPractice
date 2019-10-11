4a3b0a05b8b81209e9f25656ce1304bb939388c4
sfox <kempofighter@gmail.com>
2019-10-10 19:58:35 -0700

    Capture all of the strings necessary for the lesson 2 assignment.  Still need to setup groups for PAN ID.  This commit captures all of the superfluous spaces.

--- UCSD_Assignments/Lesson2Regex/src/main/java/com/shawnfox/java4/regex/Driver.java
+++ UCSD_Assignments/Lesson2Regex/src/main/java/com/shawnfox/java4/regex/Driver.java
@@ -1,14 +1,12 @@
 package com.shawnfox.java4.regex;
 
-import java.io.BufferedReader;
-import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
-import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 /**
  * Test driver class for Lesson 2 regular expressions.
@@ -17,50 +15,56 @@ import java.util.stream.Collectors;
  *
  */
 public class Driver {
-
+   private static final String fileName = "neighbor-dump.txt";
    private static final String pandIdRegex = "PANID\\s*=\\s*[a-fA-F0-9]{4}";
    private static final String macPlusRfRssiRegex = "(?<mac>[a-fA-F0-9]{16}).*(?<rfRssi>-[0-9]+\\.[0-9]+)";
 
    public static void main(String[] args) {
-      try (BufferedReader input = Files.newBufferedReader(Paths.get("neighbor-dump.txt"))) {
-         String line = null;
-         while((line = input.readLine()) != null) {
-            System.out.println(line);
+      List<String> panId = new ArrayList<String>();
+      List<String> mac = new ArrayList<String>();
+      List<String> rfrssi = new ArrayList<String>();
+      
+      System.out.println(String.format("Processed the following input file: %n%s", fileName));
+      System.out.println("Results are as follows");
+      
+      try {
+         String input = Files.readString(Paths.get(fileName), Charset.defaultCharset());
+         
+         var panIdPattern = Pattern.compile(pandIdRegex);
+         Matcher matcher = panIdPattern.matcher(input);
+         
+         while(matcher.find()) {
+            //System.out.println(matcher.group());
+            panId.add(matcher.group());
+         }
+         
+         System.out.println(String.format("- List of PAN IDs (Total of %d)", panId.size()));
+         for(String id : panId) {
+            System.out.println(id);
+         }
+         
+         var macAndRfRssi = Pattern.compile(macPlusRfRssiRegex);
+         matcher = macAndRfRssi.matcher(input);
+         
+         while(matcher.find()) {
+            mac.add(matcher.group("mac"));
+            rfrssi.add(matcher.group("rfRssi"));
+            //System.out.println(matcher.group(1));
+            //System.out.println(matcher.group(2));
+         }
+         
+         System.out.println(String.format("- List of MAC addresses (Total of %d)", mac.size()));
+         for(String id : mac) {
+            System.out.println(id);
+         }
+         
+         System.out.println("- List of RF_RSSI_M values for each MAC address");
+         for(int i = 0; i < mac.size(); ++i) {
+            System.out.println(String.format("%s %s", mac.get(i), rfrssi.get(i)));
          }
       } 
       catch (Exception e1) {
          e1.printStackTrace();
       }
-      
-      //TODO : scanner is probably not the way to go.  Better to read it into a file because I need
-      // to use the MatchResult information to find groups.  I'm not ready for streams yet.
-      try(Scanner in = new Scanner(Paths.get("neighbor-dump.txt"))) {
-         var panIdPattern = Pattern.compile(pandIdRegex);
-         //var matcher = pattern.matcher(input)
-         
-         // Match the PANIDs
-         
-           List<String> words =
-           in.findAll(panIdPattern).map(MatchResult::group).collect(Collectors.toList()); 
-           for(String word : words) { 
-              System.out.println(word); 
-              }
-      } 
-      catch (IOException e) {
-         e.printStackTrace();
-      }
-      
-      try(Scanner in = new Scanner(Paths.get("neighbor-dump.txt"))) {
-         // Match the MAC and RF_RSSI_I information
-         var macPlusRfRssiPattern = Pattern.compile(macPlusRfRssiRegex);
-         List<String> words = in.findAll(macPlusRfRssiPattern).map(MatchResult::group).collect(Collectors.toList());
-         for(String word : words) {
-            System.out.println(word);
-         }
-      } 
-      catch (IOException e) {
-         e.printStackTrace();
-      }
    }
-
 }
