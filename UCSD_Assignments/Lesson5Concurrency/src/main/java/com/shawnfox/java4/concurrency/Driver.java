c09d615890d60136e65cd5570ce361fe393d31f0
sfox <kempofighter@gmail.com>
2019-10-21 20:00:12 -0700

    Commit the initial project structure with a working hello world app and test case.

---
+++ UCSD_Assignments/Lesson5Concurrency/src/main/java/com/shawnfox/java4/concurrency/Driver.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.shawnfox.java4.concurrency;
+
+/**
+ * @author Shawn D. FOx
+ *
+ */
+public class Driver {
+
+   /**
+    * @param args
+    */
+   public static void main(String[] args) {
+      System.out.println("hello world");
+   }
+}
---
+++ UCSD_Assignments/Lesson5Concurrency/src/test/java/com/shawnfox/java4/concurrency/ThreadTests.java
@@ -0,0 +1,13 @@
+package com.shawnfox.java4.concurrency;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ThreadTests {
+
+   @Test
+   void test() {
+      assertTrue(true);
+   }
+}
