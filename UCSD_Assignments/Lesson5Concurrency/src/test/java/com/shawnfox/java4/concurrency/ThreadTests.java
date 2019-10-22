d8008bada06ea47b849af8e3bb1282af1718e1f2
sfox <kempofighter@gmail.com>
2019-10-21 22:29:33 -0700

    Commit some unit tests.

--- UCSD_Assignments/Lesson5Concurrency/src/test/java/com/shawnfox/java4/concurrency/ThreadTests.java
+++ UCSD_Assignments/Lesson5Concurrency/src/test/java/com/shawnfox/java4/concurrency/ThreadTests.java
@@ -2,12 +2,54 @@ package com.shawnfox.java4.concurrency;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 import org.junit.jupiter.api.Test;
+
+class Fibonacci implements Callable<Long> {
+
+   private long nth = 5;
+
+   @Override
+   public Long call() throws Exception {
+      return fib(nth);
+   }
+
+   long fib(long n) {
+      
+      if (n <= 1)
+         return n;
+      return fib(n - 1) + fib(n - 2);
+   }
+
+   public void setNth(long value) {
+      nth = value <= 0 ? 0 : value;
+   }
+
+}
 
 class ThreadTests {
 
    @Test
-   void test() {
-      assertTrue(true);
+   void testNoThread() throws Exception {
+      var callable = new Fibonacci();
+      assertEquals(5, callable.call()); // 5th
+      callable.setNth(0);
+      assertEquals(0, callable.call());
+      callable.setNth(1);
+      assertEquals(1, callable.call());
+      callable.setNth(9);
+      assertEquals(34, callable.call());
+   }
+   
+   @Test
+   void testFuture() throws Exception {
+      var callable = new Fibonacci();
+      callable.setNth(9);
+      var executor = Executors.newFixedThreadPool(1);
+      Future<Long> result = executor.submit(callable);
+      assertEquals(34, result.get());
    }
 }
