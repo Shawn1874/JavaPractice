882e67de87ad19a00bf15dbc4d4a4be152be5901
sfox <kempofighter@gmail.com>
2019-10-30 19:46:38 -0700

    Commit initial Lesson6Server project and a minor update to Lesson6Client and Lesson5Concurrency projects.

--- UCSD_Assignments/Lesson6NetworkingClient/src/main/java/com/shawnfox/java4/networking/client/Driver.java
+++ UCSD_Assignments/Lesson6NetworkingClient/src/main/java/com/shawnfox/java4/networking/client/Driver.java
@@ -3,7 +3,7 @@ package com.shawnfox.java4.networking.client;
 public class Driver {
 
    public static void main(String[] args) {
-      System.out.println("hello world!");
+      System.out.println("client - hello world!");
 
    }
 
---
+++ UCSD_Assignments/Lesson6NetworkingServer/src/main/java/com/shawnfox/java4/networking/server/Driver.java
@@ -0,0 +1,10 @@
+package com.shawnfox.java4.networking.server;
+
+public class Driver {
+
+   public static void main(String[] args) {
+      System.out.println("server - hello world!");
+
+   }
+
+}
