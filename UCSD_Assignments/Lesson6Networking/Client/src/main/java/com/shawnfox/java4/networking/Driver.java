7c494bc8f34b6eebee365dc8c16add6dbdb76c9f
sfox <kempofighter@gmail.com>
2019-10-30 19:17:33 -0700

    Commit initial project structure

---
+++ UCSD_Assignments/Lesson6Networking/Client/src/main/java/com/shawnfox/java4/networking/Driver.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.shawnfox.java4.networking;
+
+/**
+ * @author Shawn D. Fox
+ *
+ */
+public class Driver {
+
+   /**
+    * @param args
+    */
+   public static void main(String[] args) {
+      System.out.println("hello world!");
+
+   }
+
+}
