ecd84d75c44cbe8ce5723eab654fee15c9873988
sfox <kempofighter@gmail.com>
2019-11-05 20:32:25 -0800

    commit working assignment

--- UCSD_Assignments/Lesson6NetworkingClient/src/main/java/com/shawnfox/java4/networking/client/Driver.java
+++ UCSD_Assignments/Lesson6NetworkingClient/src/main/java/com/shawnfox/java4/networking/client/Driver.java
@@ -1,7 +1,5 @@
 package com.shawnfox.java4.networking.client;
 
-import java.io.IOException;
-import java.net.InetAddress;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
@@ -12,8 +10,15 @@ import picocli.CommandLine.ArgGroup;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
+/**
+ * This class demonstrates connecting to a server through a socket, and receiving data through
+ * the input stream of the socket.
+ * @author Shawn D. Fox
+ *
+ */
+@Command(name = "Lesson6NetworkingClient", mixinStandardHelpOptions = true, version = "1.0")
 public class Driver  implements Callable<Void> {
-   @ArgGroup(exclusive = true)
+   @ArgGroup(exclusive = false)
    ConnectionOptions options = new ConnectionOptions();
    
    static class ConnectionOptions {
@@ -28,10 +33,19 @@ public class Driver  implements Callable<Void> {
       int port = 0;
    }
    
+   /**
+    * The entry point of the application.  It parses the command line arguments, and executes a 
+    * callable task if the arguments or valid.
+    * @param args
+    */
    public static void main(String[] args) {
       System.exit(new CommandLine(new Driver()).execute(args));
    }
 
+   /**
+    * Connect to a server using the specified options.  Read the data from the input stream,
+    * and print it to the console.
+    */
    @Override
    public Void call() throws Exception {
       try (var s = new Socket(options.server, options.port);
--- UCSD_Assignments/Lesson6NetworkingServer/src/main/java/com/shawnfox/java4/networking/server/Driver.java
+++ UCSD_Assignments/Lesson6NetworkingServer/src/main/java/com/shawnfox/java4/networking/server/Driver.java
@@ -3,6 +3,7 @@ package com.shawnfox.java4.networking.server;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
@@ -15,16 +16,31 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
-@Command(name = "Lesson5NetworkingServer", mixinStandardHelpOptions = true, version = "1.0")
+/**
+ * 
+ * @author Shawn D. Fox
+ *
+ */
+@Command(name = "Lesson6NetworkingServer", mixinStandardHelpOptions = true, version = "1.0")
 public class Driver implements Callable<Void>{
 
    @Option(names = { "-p", "--Port" }, required = true, description = "The port number to listen on")
-   private static int port = 5000;
+   private static int port = 0;
    
+   /**
+    * The entry point of the application.  It parses the command line arguments, and executes a 
+    * callable task if the arguments or valid.
+    * @param args
+    */
    public static void main(String[] args) {
       System.exit(new CommandLine(new Driver()).execute(args));
    }
 
+   /**
+    * Creates a server socket on the specified port, and waits for a connection. When the 
+    * connection is accepted, a file is read and pushed through the socket to the client as
+    * UTF-8 characters.
+    */
    @Override
    public Void call() throws Exception {
       try {
@@ -32,24 +48,23 @@ public class Driver implements Callable<Void>{
          try (var s = new ServerSocket(port))
          {
             // wait for client connection
-            System.out.println("waiting for connection");
+            System.out.println("waiting for connection.");
             
             try (Socket incoming = s.accept())
             {
                OutputStream outStream = incoming.getOutputStream();
-      
-               try (BufferedReader in = new BufferedReader(new FileReader("TestData.txt")))
+               var input = Driver.class.getClassLoader().getResourceAsStream("TestData.txt");
+               try (BufferedReader in = new BufferedReader(
+                     new InputStreamReader(input, StandardCharsets.UTF_8)))
                {
                   var out = new PrintWriter(
                      new OutputStreamWriter(outStream, StandardCharsets.UTF_8),
                      true /* autoFlush */);
                   
-                  // echo client input
-                  String current;
-                  while((current = in.readLine()) != null)
-                  {
-                     out.println(current);
-                  }
+                  // Send the file to the client.
+                  System.out.println("Sending the file TestData.txt.");
+                  in.lines().forEach(line -> out.println(line));
+                  System.out.println("Finished.  Exiting.");
                }
             }
          }
