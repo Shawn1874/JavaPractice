691bd8fe40de4687c4aa40b3c5cc771a5f674d7b
sfox <kempofighter@gmail.com>
2019-11-08 10:48:41 -0800

    Check in a working hello world project as a starting point for the Lesson 7 assignment.

---
+++ UCSD_Assignments/Lesson7Database/src/main/java/com/shawnfox/java4/jdbc/Driver.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.shawnfox.java4.jdbc;
+
+/**
+ * @author Shawn D. Fox
+ *
+ */
+public class Driver {
+
+   /**
+    * @param args
+    */
+   public static void main(String[] args) {
+      System.out.println("Hello World!");
+   }
+
+}
