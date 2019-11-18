d2f250e5eb9cf4ee4bcdbb1e466620e48569c2a0
sfox <kempofighter@gmail.com>
2019-11-17 18:07:56 -0800

    Committing project structure with one parent and one executable JAR.

---
+++ UCSD_Assignments/Final-Adv-Java-Assignment-Shawn-Fox/Adv-Java-Final-Project-Application/src/main/java/com/adv/java/application/Driver.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.adv.java.application;
+
+/**
+ * @author Shawn D. Fox
+ *
+ */
+public class Driver {
+
+   /**
+    * @param args
+    */
+   public static void main(String[] args) {
+      System.out.println("Hello World");
+   }
+
+}
